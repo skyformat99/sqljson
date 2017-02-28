61f8e53825c1bec2f0ae374ef2cb869d1433fa20
Teodor Sigaev <teodor@sigaev.ru>
2017-02-28 16:26:59 +0300

    add header for jsonpath type

--- src/backend/utils/adt/jsonpath.c
+++ src/backend/utils/adt/jsonpath.c
@@ -34,7 +34,8 @@ static int
 flattenJsonPathParseItem(StringInfo buf, JsonPathParseItem *item,
 						 bool forbiddenRoot)
 {
-	int32	pos = buf->len - VARHDRSZ; /* position from begining of jsonpath data */
+	/* position from begining of jsonpath data */
+	int32	pos = buf->len - JSONPATH_HDRSZ;
 	int32	chld, next;
 
 	check_stack_depth();
@@ -166,7 +167,7 @@ jsonpath_in(PG_FUNCTION_ARGS)
 	initStringInfo(&buf);
 	enlargeStringInfo(&buf, 4 * len /* estimation */);
 
-	appendStringInfoSpaces(&buf, VARHDRSZ);
+	appendStringInfoSpaces(&buf, JSONPATH_HDRSZ);
 
 	if (jsonpath != NULL)
 	{
@@ -174,6 +175,7 @@ jsonpath_in(PG_FUNCTION_ARGS)
 
 		res = (JsonPath*)buf.data;
 		SET_VARSIZE(res, buf.len);
+		res->header = JSONPATH_VERSION;
 
 		PG_RETURN_JSONPATH(res);
 	}
@@ -423,7 +425,8 @@ jsonpath_out(PG_FUNCTION_ARGS)
 void
 jspInit(JsonPathItem *v, JsonPath *js)
 {
-	jspInitByBuffer(v, VARDATA(js), 0);
+	Assert(js->header == JSONPATH_VERSION);
+	jspInitByBuffer(v, js->data, 0);
 }
 
 void
